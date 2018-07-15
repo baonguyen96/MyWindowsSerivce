8a3521f9dd1d1940349d6aaee26163e40d27be25
baonguyen96 <baonguyendrump@gmail.com>
2018-07-14 19:17:42 -0500

    Add project files.

---
+++ MyWindowsSerivce/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace MyWindowsSerivce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var rc = HostFactory.Run(x =>                                 
+            {
+                x.Service<TopShelfDemo>(s =>                                 
+                {
+                    s.ConstructUsing(name => new TopShelfDemo());                
+                    s.WhenStarted(tc => tc.Start());                         
+                    s.WhenStopped(tc => tc.Stop());                          
+                });
+                x.RunAsLocalSystem();                                       
+
+                x.SetDescription("Sample Topshelf Host");                   
+                x.SetDisplayName("TopShelf");                                  
+                x.SetServiceName("TopShelf");
+
+//                x.BeforeInstall(settings =>
+//                {
+//                    Console.WriteLine("before install");
+//                    RunCmdCommand("install");
+//                });
+
+
+                x.AfterInstall(settings =>
+                {
+                    Console.WriteLine("after install");
+                    RunCmdCommand("start");
+                });
+                
+
+                x.BeforeUninstall(() =>
+                {
+                    Console.WriteLine("before uninstall");
+                    RunCmdCommand("stop");
+                });
+
+                Console.WriteLine("set start auto");
+                x.StartAutomatically();
+
+            });                                                             
+
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+
+        }
+
+        private static void RunCmdCommand(string command)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/C ./MyWindowsService.exe {command}";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+
+            const string path = "./Command.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(command);
+            }
+
+        }
+
+    }
+}
---
+++ MyWindowsSerivce/TopShelfDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using Microsoft.Win32;
+
+namespace MyWindowsSerivce
+{
+    class TopShelfDemo
+    {
+        readonly Timer _timer;
+
+        public TopShelfDemo()
+        {
+            _timer = new Timer(1000) {AutoReset = true};
+            _timer.Elapsed += Log;
+        }
+
+        private void Log(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            const string path = "./Log.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine($"{DateTime.Now}");
+            }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+       
+
+    }
+}
